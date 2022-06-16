70fe51c109333c5eb643b42443e3cc876e513163
flivi027 <107584382+flivi027@users.noreply.github.com>
2022-06-16 02:06:45 +0200

    Add files via upload

---
+++ programmino1.lua
@@ -0,0 +1,51 @@
+a = 30
+b = 1
+c = 10
+val = 300
+--presentazione
+print("ciao")
+print("questo e un server per roblox")
+--inizio
+while (a == 30)
+do
+    if (a <= 3) then
+        while (a >= 10) do
+          print("ciao come va la vita")
+          a = a -1
+        end
+      print("l`esecuzione e quasi finita")
+      print("lesecuzione e andata a buon fine")
+      a = a - 1
+    end
+    print("ciao mondo")
+    if (a <= 1) then
+        while (a <= 30) do
+          print("ci sono stati dei problemi")
+          a = a + 1
+        end
+      print("l`esecuzione e fallita")
+      a = a + 1
+    end
+    a = a - 3
+end
+print("ottimo andiamo avanti")
+if(b == 1)
+then
+    while(c <=10)
+    do
+      if (c == 30)
+     then
+       print("fine")
+      end
+      print("mee mo puoi andare")
+      c = c + 10
+    end
+    while false
+    do
+      if (val <= 70)
+      then
+        print(bu)
+      end
+    end
+  b = 1
+end
